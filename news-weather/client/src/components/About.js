b6fd9f45df1a478b34d5f802bfab60f6a25c106b
Kelvin Gwari <gwarikelvin@gmail.com>
2020-03-18 13:19:27 +0200

    style changes

--- news-weather/client/src/components/About.js
+++ news-weather/client/src/components/About.js
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 export default class About extends Component {
   render() {
     return (
-      <div>
+      <div style={{ height: "427em" }}>
         <h1>ABOUT PAGE</h1>
         <p>
-          This Site was designed by Kelvin Gwari in an effert to bring news and
+          This Site was designed by Kelvin Gwari in an effort to bring news and
           weather to one place.
         </p>
       </div>
--- news-weather/client/src/components/header.js
+++ news-weather/client/src/components/header.js
@@ -6,28 +6,34 @@ export default class header extends Component {
   render() {
     return (
       <div>
-        <div>
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h4 className="App-title">
-              Welcome To the Development Site Of the Coolest App In Town!!
-            </h4>
-            <div className="head">
-              <li className="is-active">
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/news">News</Link>
-              </li>
-              <li>
-                <Link to="/sport">Sports</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-            </div>
-          </header>
-        </div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h4 className="App-title">
+            Welcome To the Development Site Of the Coolest App In Town!!
+          </h4>
+          <div className="head">
+            <li className="is-active">
+              <Link to="/" replace>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/news" replace>
+                News
+              </Link>
+            </li>
+            <li>
+              <Link to="/sport" replace>
+                Sports
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" replace>
+                About
+              </Link>
+            </li>
+          </div>
+        </header>
       </div>
     );
   }
