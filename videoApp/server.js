d55e32ab48c8b2ba4623514d95c5c0ab1110169c
Kelvin Gwari <gwarikelvin@gmail.com>
2020-04-15 03:35:17 +0300

    VideoApp initial commit

---
+++ videoApp/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"));
+
+let clients = 0;
+
+io.on("connection", (socket) => {
+  socket.on("NewClient", function () {
+    if (clients < 2) {
+      if (clients == 1) {
+        this.emit("CreatePeer");
+      }
+    } else {
+      this.emit("SessionActive");
+    }
+    clients++;
+  });
+  socket.on("Offer", sendOffer);
+  socket.on("Answer", sendAnswer);
+  socket.on("disconnect", Disconnect);
+});
+
+function Disconnect() {
+  if (clients > 0) {
+    clients--;
+  }
+}
+
+function sendAnswer(data) {
+  this.broadcast.emit("BackAnswer", data);
+}
+
+function sendOffer(offer) {
+  this.broadcast.emit("BackOffer", offer);
+}
+
+http.listen(port, () => console.log(`Listening on ${port}`));
